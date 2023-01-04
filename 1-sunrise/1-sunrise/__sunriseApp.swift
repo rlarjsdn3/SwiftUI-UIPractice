28c8f2a4f469121c578fc7396b5faac3d8b265f3
Zizi_Kim <rlarjsdn3@naver.com>
2023-01-04 13:43:33 +0900

    1-sunrise  initial commit

---
+++ 1-sunrise/1-sunrise/__sunriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  __sunriseApp.swift
+//  1-sunrise
+//
+//  Created by 김건우 on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct __sunriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
