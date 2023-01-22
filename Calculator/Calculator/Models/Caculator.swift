2d80e0ba121696e363fb11fe66399c43d0414329
Zizi_Kim <rlarjsdn3@naver.com>
2023-01-22 23:56:36 +0900

    Calculator 프로젝트 삭제

--- Calculator/Calculator/Models/Caculator.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Caculator.swift
-//  Calculator
-//
-//  Created by 김건우 on 2023/01/22.
-//
-
-import Foundation
--- Calculator/Calculator/Models/Digit.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Digit.swift
-//  Calculator
-//
-//  Created by 김건우 on 2023/01/22.
-//
-
-import Foundation
